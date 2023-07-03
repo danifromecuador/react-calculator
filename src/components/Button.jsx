8f1880f858914d52499f3a9f6df9eaca1583472b
dani <danifromecuador@gmail.com>
2023-07-03 17:22:18 -0500

    Install prop-types using npm install prop-types

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Button = ({ character }) => (
   <button type="button">{character}</button>
 );
+
+Button.propTypes = {
+  character: PropTypes.string.isRequired,
+};
 
 export default Button;
